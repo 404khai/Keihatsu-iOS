f45591bdf7e9139ad392a77a1457547607349616
Khai <omajeneoghenefega11@gmail.com>
2026-05-27 13:39:36 -0700

    feat: project init

---
+++ Keihatsu_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keihatsu_iOSApp.swift
+//  Keihatsu-iOS
+//
+//  Created by admin on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Keihatsu_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
